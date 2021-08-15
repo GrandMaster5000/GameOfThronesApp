45b0242272de4d100430ee0084627511f1eb1db2
Anatoliy Korobskih <82397418+tolik22869@users.noreply.github.com>
2021-08-15 12:14:09 +0300

    prod v1.0.0

---
+++ build/precache-manifest.17a94b689d4a3ceb46cceb63f75812df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ad9f531a173e3056bdb",
+    "url": "/static/css/main.b19baa22.chunk.css"
+  },
+  {
+    "revision": "7ad9f531a173e3056bdb",
+    "url": "/static/js/main.7ad9f531.chunk.js"
+  },
+  {
+    "revision": "3ce1670272557e6d469e",
+    "url": "/static/css/1.99b87471.chunk.css"
+  },
+  {
+    "revision": "3ce1670272557e6d469e",
+    "url": "/static/js/1.3ce16702.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "662fab20740d068c12f3fdc798b59374",
+    "url": "/index.html"
+  }
+];
